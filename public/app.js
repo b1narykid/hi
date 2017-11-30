a1b328f1a1203039859e44c068c1da55aadf0558
Ivan Trubach <mr.trubach@icloud.com>
2017-12-01 02:35:54 +0300

    Autoscroll messages, handle some connection errors

--- public/app.js
+++ public/app.js
@@ -1,49 +1,67 @@
+prependChild = function(container, element) {
+    if (container.firstChild)
+        return container.insertBefore(element, container.firstChild);
+    else
+        return container.appendChild(element);
+};
+
 document.getElementById("usr").onkeyup = function(e) {
   if (e.keyCode == 13) {
     join();
   }
 };
+
 function join() {
   var username = document.getElementById("usr").value;
   var proto = location.protocol == "http:" ? "ws" : "wss";
-  var ws = new WebSocket(proto + "://" + window.location.host + "/ws?username=" + username);
+  var url = proto + "://" + window.location.host +
+            "/ws?username=" + encodeURIComponent(username);
+  var ws = new WebSocket(url);
+  window.onbeforeunload = function() { ws.close() };
 
-  var joinr = document.getElementById("join");
-  joinr.style.display = "none";
+  ws.onerror = function(e) {
+    alert("Connection error")
+  }
 
-  var chat = document.getElementById("chat");
-  chat.style.display = "block";
+  ws.onopen = function() {
+    var msgs = document.getElementById("messages");
+    document.getElementById("join").style.display = "none";
+    document.getElementById("chat").style.display = "block";
 
-  var msgs = document.getElementById("messages");
+    ws.onmessage = function(e) {
+      var msg = JSON.parse(e.data);
 
-  ws.addEventListener('message', function(e) {
-    var msg = JSON.parse(e.data);
+      var node = document.createElement("p");
 
-    var node = document.createElement("p");
-    var k = document.createElement("span");
-    k.setAttribute("class", msg["Meta"] ? "channel" : "user");
-    k.appendChild(document.createTextNode(msg["From"]));
-    node.appendChild(k);
-    var v = document.createElement("span");
-    v.setAttribute("class", "message");
-    v.appendChild(document.createTextNode(msg["Message"]));
-    node.appendChild(v);
-    msgs.appendChild(node);
-  });
+      var k = document.createElement("span");
+      k.setAttribute("class", msg["Meta"] ? "channel" : "user");
+      k.appendChild(document.createTextNode(msg["From"]));
+      node.appendChild(k);
 
-  var roomInsert = document.getElementById("room");
+      node.appendChild(document.createTextNode(' '));
 
-  document.getElementById("send").onkeyup = function(e) {
-    if (e.keyCode == 13) {
-      ws.send(
-        JSON.stringify({
-            From: username,
-            Message: e.target.value,
-            Room: roomInsert.value,
-            Meta: false,
-        })
-      );
-      e.target.value = "";
+      var v = document.createElement("span");
+      v.setAttribute("class", "message");
+      v.appendChild(document.createTextNode(msg["Message"]));
+      node.appendChild(v);
+
+      prependChild(msgs, node);
+    };
+
+    var roomInsert = document.getElementById("room");
+
+    document.getElementById("send").onkeyup = function(e) {
+      if (e.keyCode == 13) {
+        ws.send(
+          JSON.stringify({
+              From: username,
+              Message: e.target.value,
+              Room: roomInsert.value,
+              Meta: false,
+          })
+        );
+        e.target.value = "";
+      }
     }
-  };
+  }
 }
